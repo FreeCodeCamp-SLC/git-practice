579bcd3cb194e8fbb9236167bba4bed25613bc6b
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-09-24 12:07:29 -0600

    finished fizzbuzz

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
+var userInput = +prompt('What number would you like to count to?');
+// console.log(typeof userInput);
 
+function fizzy(number) {
+  if(number %3 === 0) {
+    return true;
+  } else {
+    return false;
+
+  };
+};
+
+function buzzy(number) {
+  if(number %5 === 0) {
+    return true;
+  }else {
+    return false;
+  };
+};
+
+function fizzbuzz(number) {
+  parseInt(number);
+
+  for (var i=0; i<=number; i++) {
+    if(buzzy(i) && buzzy(i)) {
+      $('.output').append('<p>FizzBuzz</p>');
+    } else { if (buzzy(i)) {
+      $('output').append
+    }}
+    })
+  }
+}
 });

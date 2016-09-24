9b89638461e4f8e3c21a1cfe3e6949e312b01506
Serge Pokrovskii <spokrovskey@gmail.com>
2016-09-24 12:06:24 -0600

    fizBuzz

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 $(document).ready(function() {
+ var userInput = +prompt('What number?');
 
+ function fizz(number) {
+   if(number % 3 === 0) {
+     return true;
+   } else {
+     return false;
+   };
+ };
+
+ function buzz(number) {
+   if(number % 5 === 0) {
+     return true;
+   } else {
+     return false;
+   };
+ };
+
+function fizBuzz (number) {
+  parseInt(number);
+
+  for(var i= 1; i <= number; i++) {
+    if(buzz(i) && fizz(i)) {
+      $('.output').append('<p>FizzBuzz</p>');
+    } else if (buzz(i)) {
+      $('.output').append('<p>buzz</p>');
+    } else if (fizz(i)) {
+      $('.output').append('<p>fizz</p>');
+    } else {
+      $('.output').append('<p>' + i + '</p>');
+    };
+  };
+};
+
+while (userInput % 1 !=0) {
+  alert('please provide other number');
+  uaserInput = prompt('what number would you like to check?');
+
+}
+ fizBuzz(userInput);
 });
